71a1762f5581ec591a21772e595f3f47e3466125
welermoura <155203871+welermoura@users.noreply.github.com>
2025-11-10 12:51:33 -0300

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ADTree from './components/ADTree';
+
+function App() {
+  return (
+    <div className="App">
+      <ADTree />
+    </div>
+  );
+}
+
+export default App;
