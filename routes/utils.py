68c6cee8783c96035bae5101f86dd2ceabd074b9
Weler Moura <welerMS@comolatti.com.br>
2026-06-03 11:16:07 -0300

    feat: aumentar limite de requisicoes (rate limit) para evitar bloqueio durante uso normal

--- routes/utils.py
+++ routes/utils.py
@@ -9,7 +9,7 @@ import logging
 # Instância global do Limiter para ser usada em Blueprints
 limiter = Limiter(
     key_func=get_remote_address,
-    default_limits=["200 per day", "50 per hour"],
+    default_limits=["10000 per day", "3000 per hour"],
     storage_uri="memory://",
 )
 
